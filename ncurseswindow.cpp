e4713bc77d1f3e59a27849c0d54ebd73e740db16
Siti Ayuni <92244072+sitiayuni@users.noreply.github.com>
2021-12-11 11:16:39 +0700

    Add files via upload

---
+++ ncurseswindow.cpp
@@ -0,0 +1,15 @@
+#include <ncurses.h>
+
+int main() {
+  initscr();
+  curs_set (0);
+
+  WINDOW *win = newwin(20, 35, 4, 8); // panjang = 20, lebar = 35, dimulai pada baris 4 kolom 8
+    refresh(); 
+    
+    box(win, 0, 0);
+    wrefresh(win);
+    
+    getch(); //menahan layar
+    endwin(); //mengakhiri program 
+} 
