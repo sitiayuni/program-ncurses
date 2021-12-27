934b4c1e5497af323728706f5c6c721b3c50dbce
Siti Ayuni <92244072+sitiayuni@users.noreply.github.com>
2021-12-27 10:05:47 +0700

    Add files via upload

---
+++ ncursessederhana.cpp
@@ -0,0 +1,31 @@
+#include <ncurses/ncurses.h>
+#include <windows.h>
+using namespace std;
+
+int main (){
+	
+	initscr();
+	
+	mvprintw(5,7," Loading");
+
+	
+	mvprintw(5,15, " .... ");
+	refresh();
+    Sleep (1000);
+	
+	mvprintw(5,20, " .... ");
+	refresh();
+	Sleep (1000);
+	
+	mvprintw(5,25, " ....");
+	refresh();
+	Sleep(1000);
+	
+	mvprintw(5,30," ....");
+	refresh();
+	Sleep(1000);
+	
+	getch();
+	endwin();
+	
+}
